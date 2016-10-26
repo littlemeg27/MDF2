06829a45d8107391e02b66c6107a825b3bb04c41
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2016-10-26 17:14:15 -0400

    Project 4

---
+++ MDF2Project4/MDF2Project4Tests/MDF2Project4Tests.m
@@ -0,0 +1,34 @@
+//
+//  MDF2Project4Tests.m
+//  MDF2Project4Tests
+//
+//  Created by William Pavlinchak on 4/28/14.
+//  Copyright (c) 2014 Brenna Pavlinchak. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MDF2Project4Tests : XCTestCase
+
+@end
+
+@implementation MDF2Project4Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
